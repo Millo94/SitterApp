69209a7f8a60d71e5fb4252a13ede6c5ffe013cc
Looprano <33062083+Looprano@users.noreply.github.com>
2018-05-11 14:23:57 +0200

    1 version Login Page

---
+++ app/src/main/java/it/uniba/di/sms/sitterapp/LoginActivity.java
@@ -0,0 +1,29 @@
+package it.uniba.di.sms.sitterapp;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText usernameEt,passwordEt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        usernameEt = (EditText) findViewById(R.id.lblUsername);
+        passwordEt = (EditText) findViewById(R.id.lblPassword);
+    }
+
+    public void onLoign(View view){
+        String username = usernameEt.getText().toString();
+        String password = passwordEt.getText().toString();
+        String type = "login";
+        Backgroundworker backgroundworker = new Backgroundworker(this);
+        backgroundworker.execute(type,username,password);
+    }
+
+}
