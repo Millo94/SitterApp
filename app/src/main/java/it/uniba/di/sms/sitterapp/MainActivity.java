fa421a209d8f2baa568e9bc7f0d616614483ac3e
frappe28 <f.deperte1@studenti.uniba.it>
2018-05-12 10:39:32 +0200

    inizio sitter registration

--- app/src/main/java/it/uniba/di/sms/sitterapp/MainActivity.java
+++ app/src/main/java/it/uniba/di/sms/sitterapp/MainActivity.java
@@ -1,13 +1,31 @@
 package it.uniba.di.sms.sitterapp;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SitterRegistrationFragment.OnFragmentInteractionListener {
+
+
+    public void onFragmentInteraction(Uri uri) {
+        return;
+    }
+
+    SitterRegistrationFragment sitterFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new SitterRegistrationFragment()).commit();
+        //sitterFrag = (SitterRegistrationFragment) getFragmentManager().findFragmentById(R.id.container);
+        /*FragmentManager fragMenager = getFragmentManager();
+        FragmentTransaction fragTransaction = fragMenager.beginTransaction();
+        SitterRegistrationFragment fragSitter = (SitterRegistrationFragment) findViewById(R.id.);*/
+
     }
 }
