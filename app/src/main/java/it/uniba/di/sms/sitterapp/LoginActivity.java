e2ed5b9f93b0ee787342fd6021f89661941a044a
Looprano <33062083+Looprano@users.noreply.github.com>
2018-05-12 13:03:37 +0200

    Server version Login Page btn create acount

--- app/src/main/java/it/uniba/di/sms/sitterapp/LoginActivity.java
+++ app/src/main/java/it/uniba/di/sms/sitterapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package it.uniba.di.sms.sitterapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +23,10 @@ public class LoginActivity extends AppCompatActivity {
         String type = "login";
         Backgroundworker backgroundworker = new Backgroundworker(this);
         backgroundworker.execute(type,username,password);
+    }
+
+    public void createAccount(View view){
+
     }
 
 }
