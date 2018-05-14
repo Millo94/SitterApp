4259080c9a724a8d4d9c9a79c2953e4aae45744e
eladi96 <e.ladisa2@studenti.uniba.it>
2018-05-14 10:41:14 +0200

    Aggiustato il manifest

--- app/src/main/java/it/uniba/di/sms/sitterapp/SitterRegistrationFragment.java
+++ app/src/main/java/it/uniba/di/sms/sitterapp/SitterRegistrationFragment.java
@@ -2,7 +2,10 @@ package it.uniba.di.sms.sitterapp;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -26,11 +29,29 @@ import java.util.Calendar;
 
 
 public class SitterRegistrationFragment extends Fragment {
+
     ArrayList<EditText> lista  = new ArrayList<>();
+
+    //Date Picker variables
     EditText dataNascita;
+    static final int DATE_DIALOG_ID = 0;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+
+    // the callback received when the user "sets" the date in the dialog
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+        new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mYear = year;
+                mMonth = monthOfYear;
+                mDay = dayOfMonth;
+                updateDisplay();
+            }
+        };
 
     View view;
-
     EditText usernameET,passwordET,confPasswordET,nameET,surnameET,emailET,phoneET;
     RadioGroup radioBtnGender;
     RadioButton male,female;
@@ -53,8 +74,26 @@ public class SitterRegistrationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_sitter_registration, container, false);
-
+        // Inizializza i componenti della vista
         initialization();
+
+        // On focus della data
+        dataNascita.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                onCreateDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        //Codice professore
+        // get the current date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // display the current date (this method is below)
+        updateDisplay();
 
 
         confRegistation.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +112,28 @@ public class SitterRegistrationFragment extends Fragment {
         });
 
         return view;
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this.getContext(),
+                        mDateSetListener,
+                        mYear, mMonth, mDay);
+        }
+        return null;
+    }
+
+
+
+
+    //Update la data
+    private void updateDisplay() {
+        dataNascita.setText(
+                new StringBuilder()
+                        .append(mDay).append("-")
+                        .append(mMonth + 1).append("-")
+                        .append(mYear).append(" "));
     }
 
 
